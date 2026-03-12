549245479a42a77385ab0b5633d3b3d7bdc95146
thelope <thelopeosrs@gmail.com>
2026-03-12 14:39:25 -0400

    fix: widget converter copy button

--- docs/javascripts/widgetConverter.js
+++ docs/javascripts/widgetConverter.js
@@ -1,8 +1,8 @@
 /*
 mapping file format:
 [
-	[old_id, new_id],
-	[old_id, new_id]
+    [old_id, new_id],
+    [old_id, new_id]
 ]
 
 if new_id is null, the highlight will be removed.
@@ -11,59 +11,59 @@ if old_id is not highlighted, nothing is done
 */
 
 function convertId(widgetId, mappings) {
-	for (mapping of mappings) {
-		var [oldId, newId] = mapping;
-		if (oldId === widgetId.componentId) {
-			if (newId) {
-				return {
-					...widgetId,
-					componentId: newId,
-				};
-			} else {
-				return null;
-			}
-		}
-	}
-	return widgetId;
+    for (mapping of mappings) {
+        var [oldId, newId] = mapping;
+        if (oldId === widgetId.componentId) {
+            if (newId) {
+                return {
+                    ...widgetId,
+                    componentId: newId,
+                };
+            } else {
+                return null;
+            }
+        }
+    }
+    return widgetId;
 }
 
-var convertButton = document.getElementById("convert-widgets");
-var copyButton = document.getElementById("copy-onverted");
+var convertButton = document.getElementById("convert");
+var copyButton = document.getElementById("copy");
 
 convertButton.addEventListener('click', () => {
-    var tagsBefore = document.getElementById("tags-before");
+    var detailsBefore = document.getElementById("details-before");
     var mappingFile = document.getElementById("mapping-file");
-    var tagsAfter = document.getElementById("tags-after");
+    var detailsAfter = document.getElementById("details-after");
 
-    var tags = JSON.parse(tagsBefore.value);
-		var mappings = JSON.parse(mappingFile.value);
+    var tags = JSON.parse(detailsBefore.value);
+        var mappings = JSON.parse(mappingFile.value);
 
     var arr = tags.map(tag => {
-			const widgets = tag.widgetIds
-				? tag.widgetIds.map(widgetId => convertId(widgetId, mappings)).filter(widgetId => widgetId)
-				: null;
+            const widgets = tag.widgetIds
+                ? tag.widgetIds.map(widgetId => convertId(widgetId, mappings)).filter(widgetId => widgetId)
+                : null;
 
-			if (!tag.widgetIds && !widgets) {
-				return tag;
-			}
+            if (!tag.widgetIds && !widgets) {
+                return tag;
+            }
 
-			return {
-				...tag,
-				widgetIds: widgets,
-			}
+            return {
+                ...tag,
+                widgetIds: widgets,
+            }
     });
 
-    var converted = JSON.stringify(arr);
+    var converted = JSON.stringify(arr, null , 4);
 
-    tagsAfter.style.display = "block";
-    tagsAfter.value = converted;
+    detailsAfter.style.display = "block";
+    detailsAfter.value = converted;
 
     var tooltip = document.getElementById("convertTooltip");
     tooltip.innerHTML = "Fixed";
 });
 
 copyButton.addEventListener('click', () => {
-    var copyText = document.getElementById("tags-after");
+    var copyText = document.getElementById("details-after");
 
     copyText.select();
     copyText.setSelectionRange(0, 99999);
